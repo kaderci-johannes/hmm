05d2744680203446746adf476ddbfc00ea01df86
Ohannes Kamer Koseyan <kaderci.johannes@gmail.com>
2020-10-25 17:45:10 +0100

    update

--- HMUMU.C
+++ HMUMU.C
@@ -1,31 +1,34 @@
 #include <iostream>
 #include <TLorentzVector.h>
 
-void HMUMU(const int N)
-{
-char path[1024], outpath[1024];
-
 // mc=10: M>50 (DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/MINIAODSIM)
 // mc=11: 105<M<160 (DYJetsToLL_M-105To160_TuneCP5_PSweights_13TeV-amcatnloFXFX-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/MINIAODSIM)
 
+void HMUMU(const int N){
+
+gROOT->SetBatch(kTRUE);
+
+char path[1024], outpath[1024];
+TLorentzVector u1,u2;
+//Int_t err=0;
+
 TFile *file = new TFile;
+TFile *out = new TFile("./results.root","recreate");
 TTree *hmm = new TTree;
+
 TCanvas *c1 = new TCanvas;
 TCanvas *c2 = new TCanvas;
 TCanvas *c3 = new TCanvas;
 TCanvas *c4 = new TCanvas;
 TCanvas *c5 = new TCanvas;
 TCanvas *c6 = new TCanvas;
+
 TH1F *h10 = new TH1F("mc = 10","mc = 10",100,70,170);
 TH1F *h11 = new TH1F("mc = 11","mc = 11",100,70,170);
 TH1F *h120gev = new TH1F("m = 120","m = 120",100,70,170);
 TH1F *h125gev = new TH1F("m = 125","m = 125",100,70,170);
 TH1F *h130gev = new TH1F("m = 130","m = 130",100,70,170);
 TH1F *h00 = new TH1F("mc != 0, 10, 11","mc != 0, 10, 11",100,70,170);
-
-TLorentzVector u1,u2;
-
-//Int_t err=0;
 
 for(Int_t i=0; i<N; i++)
 {
@@ -60,6 +63,21 @@ for(Int_t i=0; i<N; i++)
 		else if(*mc) h00->Fill((u1+u2).M(),(*weight)*60000);
 	}
 }
+out->cd();
+h10->SetXTitle("M (GeV)");
+h10->Write();
+h11->SetXTitle("M (GeV)");
+h11->Write();
+h00->SetXTitle("M (GeV)");
+h00->Write();
+h120gev->SetXTitle("M (GeV)");
+h120gev->Write();
+h125gev->SetXTitle("M (GeV)");
+h125gev->Write();
+h130gev->SetXTitle("M (GeV)");
+h130gev->Write();
+out->Write();
+out->Close();
 
 c1->cd();
 h10->Draw();
